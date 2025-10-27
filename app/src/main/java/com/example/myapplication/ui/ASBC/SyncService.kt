5c50e9ab5051f0d3d39951984d8c2ec19e1cb4e7
rikai-sangbui <sang.bui@rikai.technology>
2025-10-27 19:57:35 +0700

    erw

--- app/src/main/java/com/example/myapplication/ui/ASBC/SyncService.kt
+++ app/src/main/java/com/example/myapplication/ui/ASBC/SyncService.kt
@@ -4,15 +4,22 @@ import android.app.Service
 import android.content.ContentValues
 import android.content.Intent
 import android.os.IBinder
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.cancel
 import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import kotlin.coroutines.coroutineContext
 
 class SyncService: Service() {
     override fun onBind(p0: Intent?): IBinder? = null
+    private val scope = CoroutineScope(Dispatchers.Default + SupervisorJob())
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        Thread {
-            Thread.sleep(3000)
+        scope.launch {
+            delay(2000)
             val msg = "Sync data success ${System.currentTimeMillis()}"
 
             // ✅ Lưu vào ContentProvider
@@ -23,7 +30,15 @@ class SyncService: Service() {
             val br = Intent("com.example.myapp.ABC")
             br.putExtra("data", msg)
             sendBroadcast(br)
-        }.start()
+
+            // tự stop
+            stopSelf()
+        }
         return START_NOT_STICKY
+    }
+
+    override fun onDestroy() {
+        scope.cancel()
+        super.onDestroy()
     }
 }
