b2adc979c75855349beafa6edd664a7ec887beaa
sanggithub@gmail.com <Ranmathuat@11>
2025-10-29 08:28:56 +0700

    fix comment + search

---
+++ app/src/main/java/com/example/myapplication/test5.kt
@@ -0,0 +1,32 @@
+package com.example.myapplication
+
+class Lazzylate324(){
+
+     var color = 7 * 44
+         get() = field*44
+         set(value) {
+            color = color * value
+             changeColorOfButton()
+         }
+     fun changeColorOfButton() {
+
+     }
+}
+
+val loading = "a"
+
+enum class State(val a:String){
+
+    LOADING("a"), BBB("b");
+
+    var nj : String =""
+
+}
+
+
+fun main() {
+    println(loading)
+    println(loading)
+}
+
+
---
+++ app/src/main/java/com/example/myapplication/util/sadas.kt
@@ -0,0 +1,5 @@
+package com.example.myapplication.util
+
+fun main(){
+    var a = Profile33(99)
+}
